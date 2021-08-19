3d5b49adf79096241ee1898886a930414be81840
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-08-19 12:17:19 +0300

    add

--- models/index.js
+++ models/index.js
@@ -5,9 +5,14 @@ const gamesModels = require('./games/model');
 const userModel = require('./users');
 const Collection = require('./data-collection');
 
-const DATABASE_URL = process.env.DATABASE_URL;
-let sequelizeOptions = { dialectOptions: { ssl: { require: true, rejectUnauthorized: false, } } };
-const sequelize = new Sequelize(DATABASE_URL , sequelizeOptions);
+// const DATABASE_URL = process.env.DATABASE_URL;
+// let sequelizeOptions = { dialectOptions: { ssl: { require: true, rejectUnauthorized: false, } } };
+// const sequelize = new Sequelize(DATABASE_URL , sequelizeOptions);
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL||'postgres://localhost:5432/nedalalashqar';
+// const DATABASE_URL = process.env.DATABASE_URL 
+
+const sequelize = new Sequelize(DATABASE_URL);
+
 const games = gamesModels(sequelize, DataTypes);
 
 module.exports = {
